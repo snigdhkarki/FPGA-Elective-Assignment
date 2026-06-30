6712220be5ecf0159d3d76a7a66d2a849247fc77
snigdh <snigdhkarki@gmail.com>
2026-06-30 21:51:59 +0545

    final change

--- eightbitALU/tb.v
+++ eightbitALU/tb.v
@@ -1,13 +1,9 @@
-// ---------------------------------------------------------
-// Testbench (Updated for Shift Multiplier)
-// ---------------------------------------------------------
 `timescale 1ns / 1ps
 
 module tb_alu;
-    // Inputs
     reg [7:0] a;
     reg [7:0] b;
-    reg [1:0] sel;
+    reg [2:0] sel;
 
     wire [15:0] result;
 
@@ -19,18 +15,39 @@ module tb_alu;
     );
 
     initial begin
-        a = 8'd25;   b = 8'd10;   sel = 2'b00;
+        a = 8'd25;   b = 8'd10;   sel = 3'b000;
         #10;
         $display("ADD    : %d + %d = %d", a, b, result);
 
-        a = 8'd50;   b = 8'd15;   sel = 2'b01;
+        a = 8'd50;   b = 8'd15;   sel = 3'b001;
         #10;
         $display("SUB    : %d - %d = %d", a, b, result);
 
         // 3 * (2^8) = 3 * 256 = 768
-        a = 8'd3;    b = 8'd8;    sel = 2'b10;
+        a = 8'd3;    b = 8'd8;    sel = 3'b010;
         #10;
         $display("2^n MUL: %d * 2^%d = %d", a, b, result);
+
+        a = 8'd50;   b = 8'd15;   sel = 3'b011;
+        #10;
+        $display("AND    : %d AND %d = %d", a, b, result);
+
+        a = 8'd50;   b = 8'd15;   sel = 3'b100;
+        #10;
+        $display("OR    : %d OR %d = %d", a, b, result);
+
+        a = 8'd50;   b = 8'd15;   sel = 3'b101;
+        #10;
+        $display("XOR    : %d XOR %d = %d", a, b, result);
+
+        a = 8'd50;   b = 8'd15;   sel = 3'b110;
+        #10;
+        $display("NAND    : %d NAND %d = %d", a, b, result);
+
+        a = 8'd50;   b = 8'd15;   sel = 3'b111;
+        #10;
+        $display("NOR    : %d NOR %d = %d", a, b, result);
+
 
 
         $finish;
--- eightbitALU/top.v
+++ eightbitALU/top.v
@@ -1,7 +1,7 @@
 module alu_top (
     input  [7:0] a, 
     input  [7:0] b, 
-    input  [1:0] sel, 
+    input  [2:0] sel, 
     output reg [15:0] result
 );
     wire [7:0] sum_out, diff_out;
@@ -12,11 +12,17 @@ module alu_top (
     sub_8bit        subtractor (.a(a), .b(b), .diff(diff_out), .bout(bout));
     shift_mult_8bit shifter    (.a(a), .b(b), .prod(mult_out));
 
+
     always @(*) begin
         case (sel)
-            2'b00: result = sum_out;  
-            2'b01: result = diff_out; 
-            2'b10: result = mult_out;         
+            3'b000: result = sum_out;  
+            3'b001: result = diff_out; 
+            3'b010: result = mult_out;
+            3'b011: result = a & b;
+            3'b100: result = a | b;
+            3'b101: result = a ^ b;
+            3'b110: result = ~ (a & b);
+            3'b111: result = ~ (a | b);         
             default: result = 16'b0;
         endcase
     end
